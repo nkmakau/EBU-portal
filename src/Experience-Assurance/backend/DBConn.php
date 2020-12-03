273d4d5037cb6f4d886ca878e6fc2d8f55b38ee1
noshmak <noshmak@gmail.com>
2020-12-03 21:59:25 +0300

    Added DB connection file

--- src/Experience-Assurance/backend/DBConn.php
+++
@@ -1,5 +0,0 @@
-<?php
-Set $eConn = Server.CreateObject("ADODB.Connection");
-$eConnString="Provider=mysql.1;Password=sfcintranet;Persist Security Info=True;User ID=sfcintranet;Initial Catalog=EBUDatabase;Data Source=localhost";
-$eConn.$Open $eConnString; 
-?>
---
+++ src/Experience-Assurance/backend/cond.php
@@ -0,0 +1,11 @@
+<?php
+
+$mysql_host = 'localhost';
+$mysql_dbname= 'cmintranet';
+$mysql_user = 'eaintranet';
+$mysql_password = 'K@jiado78??';
+
+$link = mysqli_connect($mysql_host, $mysql_user, $mysql_password,$mysql_dbname) or die("Unable to Connect to server");
+mysqli_select_db($link,$mysql_dbname) or die("Could not open the database");
+
+?>
